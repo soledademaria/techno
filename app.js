1ac90f2b4e41cab8cec2c08c6ed07d8aed6a7a00
soledademaria <solmmaria50@gmail.com>
2022-06-20 23:14:58 -0300

    configuração inicial do projeto

---
+++ app.js
@@ -0,0 +1,20 @@
+const vm = new Vue({
+    el: '#app',
+    data(){
+        return{
+            products: []
+        }
+    },
+    methods:{
+        fetchProducts(){
+            fetch("./api/products.json")
+            .then(res => res.json())
+            .then(res => {
+                this.products = res
+            })
+        }
+    },
+    created(){
+        this.fetchProducts()
+    }
+})
