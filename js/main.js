1463849b2325c05b266417e31514e7f081e654ac
soledademaria <solmmaria50@gmail.com>
2022-07-31 23:48:02 -0300

    ajustes finais na modal

--- js/main.js
+++ js/main.js
@@ -1,29 +1,44 @@
 const vm = new Vue({
-    el: '#app',
-    data(){
-        return{
-            products: [],
-            productDetail: false
-        }
+  el: "#app",
+  data() {
+    return {
+      products: [],
+      productDetail: false,
+    };
+  },
+  filters: {
+    currencyType(value) {
+      return value.toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL",
+      });
     },
-    filters:{
-        currencyType(value){
-            return value.toLocaleString("pt-br", {style: "currency", currency: "BRL"})
-        }
+  },
+  methods: {
+    fetchProducts() {
+      fetch("./api/products.json")
+        .then((res) => res.json())
+        .then((res) => (this.products = res));
     },
-    methods: {
-        fetchProducts(){
-            fetch("./api/products.json")
-            .then(res => res.json())
-            .then(res => this.products = res)
-        },
-        getProductItem(id){
-            fetch(`./api/products/${id}/dados.json`)
-            .then(res => res.json())
-            .then(res => this.productDetail = res)
-        }
+    openModal(id){
+        this.getProductItem(id);
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
     },
-    created(){
-        this.fetchProducts()
-    }
-})
+    getProductItem(id) {
+      fetch(`./api/products/${id}/dados.json`)
+        .then((res) => res.json())
+        .then((res) => (this.productDetail = res));
+    },
+    closeModal({ target, currentTarget }) {
+      if (target === currentTarget) {
+        this.productDetail = false;
+      }
+    },
+  },
+  created() {
+    this.fetchProducts();
+  },
+});
